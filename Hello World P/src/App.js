07bbc41d11f5945a62f6bc42c0039b1e72a35d2a
TharllesKawa <tharlleskawa22@gmail.com>
2022-08-31 09:20:29 -0300

    secound commit

---
+++ Hello World P/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import logo from '../src/logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <img src={logo} alt="" />
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
